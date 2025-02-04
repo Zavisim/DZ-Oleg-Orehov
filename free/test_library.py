8ec343f41ba2c1d79c8b86d9d993fe35b5d77ea1
Zavisim <2bru.ru@gmail.com>
2025-02-04 09:42:14 +0300

    h

--- free/test_library.py
+++ free/test_library.py
@@ -9,7 +9,7 @@ def library() -> Library:
 
 class TestLibrary:
     def test_add_book(self, library: Library):
-        """Тестируем добавление книги в каталог."""
+        """Тест добавления книги"""
         library.add_book("Book1", "Author1")
         assert (
             "Book1" in library.catalog and
@@ -17,41 +17,41 @@ class TestLibrary:
         )
 
     def test_add_replace_book(self, library: Library):
-        """Проверяем, что нельзя добавить книгу с таким же названием."""
+        """Проверка на повтор"""
         library.add_book("Book1", "Author1")
         with pytest.raises(ValueError):
             library.add_book("Book1", "Author1")
 
     def test_remove_book(self, library: Library):
-        """Тестируем удаление книги."""
+        """Тест удаления"""
         library.add_book("Book1", "Author1")
         library.remove_book("Book1")
         assert "Book1" not in library.catalog
 
     def test_remove_book_not_exist(self, library: Library):
-        """Тестируем удаление несуществующей книги."""
+        """Тест удаления того, чего нет"""
         with pytest.raises(ValueError):
             library.remove_book("Trash")
 
     def test_find_book(self, library: Library):
-        """Тестируем поиск книги по названию."""
+        """Тест поиска книги по названия"""
         library.add_book("Book1", "Author1")
         result = library.find_book("Book1")
         assert result == "Title: Book1, Author: Author1"
 
     def test_find_book_not_exist(self, library: Library):
-        """Тестируем поиск несуществующей книги."""
+        """Тест поиска книги, которой нет"""
         result = library.find_book("Trash")
         assert result is None
 
     def test_list_books(self, library: Library):
-        """Тестируем список книг в каталоге."""
+        """Тест списка книг в каталоге"""
         library.add_book("Book1", "Author1")
         library.add_book("Book2", "Author2")
         result = library.list_books()
         assert result == "Title: Book1, Author: Author1\nTitle: Book2, Author: Author2"
 
     def test_list_books_not_exist(self, library: Library):
-        """Тестируем пустой каталог."""
+        """Тестируем пустой каталог"""
         result = library.list_books()
         assert result == "No books in the catalog"
